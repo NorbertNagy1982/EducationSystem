0bba919bbed19452e59788b518408e3ec0155782
NorbertNagy1982 <nagynorbert1@gmail.com>
2024-01-09 06:48:54 +0100

    spring files

---
+++ EducationSystem/src/test/java/educationSystem/example/EducationSystem/EducationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package educationSystem.example.EducationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
